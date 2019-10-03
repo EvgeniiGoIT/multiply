1281ef0dabfe95c52e11f99223cc96c608d88cf1
Evgenii <wojda007@gmail.com>
2019-10-03 09:26:21 +0300

    refactor: write function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+  first = first.split("");
+  second = second.split("");
+  let result = [];
+  for (let i = 0; i < first.length; i++) {
+    for (let j = 0; j < second.length; j++) {
+      result[i + j]
+        ? (result[i + j] += first[i] * second[j])
+        : (result[i + j] = first[i] * second[j]);
+    }
+  }
+  result = result.reverse();
+  for (let i = 0; i < result.length - 1; i++) {
+    if (result[i] >= 10) {
+      result[i + 1] += Math.floor(result[i] / 10);
+      result[i] = result[i] % 10;
+    }
+  }
+  return result.reverse().join("");
+};
